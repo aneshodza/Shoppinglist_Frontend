188409c4e37f7327f619fc13aeb6f94a365f5f4a
aneshodza <anes.hodza04@gmail.com>
2021-07-17 11:32:09 +0200

    The sign up works, and added minor changes to login

--- reactcode/src/Components/App.js
+++ reactcode/src/Components/App.js
@@ -1,16 +1,27 @@
 import LoggedInSwitch from "./CheckIfUserIsLoggedIn.js"
 import LogInWindow from "./LoginAndSignupComponents/LogInWindow.js";
 import SignUpWindow from "./LoginAndSignupComponents/SignUpWindow.js";
+import GroupList from "./LoggedInComponents/GroupList.js";
 import '../App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+
+  if (localStorage.getItem('rememberMe') === 'false') {
+    localStorage.removeItem('rememberedUsername')
+    localStorage.removeItem('rememberedPassword')
+  } else if (localStorage.getItem('rememberedUsername') === null || localStorage.getItem('rememberedPassword') === null) {
+    localStorage.setItem('rememberMe', false)
+  }
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LoggedInSwitch} />
         <Route path="/login" component={LogInWindow} />
         <Route path="/signup" component={SignUpWindow} />
+        <Route path="/my-groups" component={GroupList} />
       </Switch>
     </Router>
   );
